b730c807bbea8405a5230c1737327db042f2edf5
Wilhelm Teicke <wilhelm.teicke@googlemail.com>
2024-05-09 11:03:24 +0200

    requirements added, test added

--- board/tests.py
+++ board/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+from .models import Task, Board
 
-# Create your tests here.
+
+class CreateTaskTest(APITestCase):
+    def setUp(self):
+        # Benutzer und Token erstellen
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.token = Token.objects.create(user=self.user)
+        # Den Authorization Header für alle Testanfragen einstellen
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        # Erstellen einer Board-Instanz, wenn notwendig
+        self.board = Board.objects.create(name='Test Board')
+        self.task_data = {
+            'title': 'Test Task',
+            'description': 'Test Task Description',
+            'user': self.user.id,
+            'board': self.board.id,
+            'status': 'todo',  # Optional, da es einen Default-Wert gibt
+        }
+    def test_create_task(self):
+        response = self.client.post("/createTask/",self.task_data, format='json')
+        self.assertEqual(response.status_code, 201)
