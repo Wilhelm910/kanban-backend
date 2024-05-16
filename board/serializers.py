53ea03b9672085dea2f952e9c5e4c05a24a2ec29
Wilhelm Teicke <wilhelm.teicke@googlemail.com>
2024-05-16 08:39:58 +0200

    username validation added

--- board/serializers.py
+++ board/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Board, Task
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 class TaskSerializer(serializers.ModelSerializer):
     # user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
@@ -39,6 +40,40 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+    # class Meta:
+    #     model = User
+    #     fields = '__all__'
+    #     extra_kwargs = {
+    #         'password': {'write_only': True},
+    #         'username': {'required': False},
+    #     }
+
+    # def validate_username(self, value):
+    #     if ' ' in value:
+    #         raise serializers.ValidationError("Username should not contain spaces.")
+    #     return value
+
+    # def create(self, validated_data):
+    #     first_name = validated_data.get('first_name', '')
+    #     last_name = validated_data.get('last_name', '')
+    #     username = validated_data.get('username')
+    #     # if not username:
+    #     #     base_username = slugify(f"{first_name} {last_name}")
+    #     #     username = base_username
+    #     #     counter = 1
+    #     #     while User.objects.filter(username=username).exists():
+    #     #         username = f"{base_username}{counter}"
+    #     #         counter += 1
+
+    #     user = User(
+    #         username=username,
+    #         first_name=first_name,
+    #         last_name=last_name,
+    #         # email=validated_data.get('email', '')
+    #     )
+    #     user.set_password(validated_data['password'])
+    #     user.save()
+    #     return user
 
     def update(self, instance, validated_data):
         # instance.email = validated_data.get('email', instance.email)
